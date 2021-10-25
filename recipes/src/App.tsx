f1ef021ead1bc9764928f80a823d5aa7996e9fa5
Glagny <nikoicardo@gmail.com>
2021-10-25 11:48:47 -0600

    Create Boiler Plate.

---
+++ recipes/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+
+  
+
+  render = () => (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App;
